eeca7138e68690a5b5b012e36c0b597e3e15c27c
Rabin <rabinrow@163.com>
2017-02-21 21:04:34 +0800

    commit my Application

---
+++ RabinApplication2/app/src/main/java/com/example/rabinapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rabinapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
